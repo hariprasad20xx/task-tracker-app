f80789162a9db6aade9742e494e0660e8b724505
Hariprasad <hariprasadjnv963@gmail.com>
2026-04-18 22:03:33 +0530

    feat(repository): create TaskRepository interface

---
+++ src/main/java/com/tasktracker/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.tasktracker.repository;
+
+import com.tasktracker.domain.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    
+}
