6742fe9b2d4b35550dda2b1dfb56e4a900131f31
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-01 20:39:10 +0530

    feat(service): add listTasks method to TaskService

--- src/main/java/com/tasktracker/service/TaskService.java
+++ src/main/java/com/tasktracker/service/TaskService.java
@@ -3,6 +3,11 @@ package com.tasktracker.service;
 import com.tasktracker.domain.CreateTaskRequest;
 import com.tasktracker.domain.entity.Task;
 
+import java.util.List;
+
 public interface TaskService {
     Task createTask(CreateTaskRequest request);
+
+    List<Task> listTasks();
+
 }
