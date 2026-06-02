76e630c29d42918e75ac5a32549377d18b96ad57
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-02 21:06:24 +0530

    feat: add updateTask method to TaskService

--- src/main/java/com/tasktracker/service/TaskService.java
+++ src/main/java/com/tasktracker/service/TaskService.java
@@ -1,13 +1,17 @@
 package com.tasktracker.service;
 
 import com.tasktracker.domain.CreateTaskRequest;
+import com.tasktracker.domain.UpdateTaskRequest;
 import com.tasktracker.domain.entity.Task;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface TaskService {
     Task createTask(CreateTaskRequest request);
 
     List<Task> listTasks();
+
+    Task updateTask(UUID taskId, UpdateTaskRequest request);
 
 }
