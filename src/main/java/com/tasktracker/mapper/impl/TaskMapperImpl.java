913def71c442778f8d7a2eb1e6bced330d3c6629
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-05 21:08:43 +0530

    chore(mapper): implement UpdateTaskRequestDto mapping logic

--- src/main/java/com/tasktracker/mapper/impl/TaskMapperImpl.java
+++ src/main/java/com/tasktracker/mapper/impl/TaskMapperImpl.java
@@ -1,8 +1,10 @@
 package com.tasktracker.mapper.impl;
 
 import com.tasktracker.domain.CreateTaskRequest;
+import com.tasktracker.domain.UpdateTaskRequest;
 import com.tasktracker.domain.dto.CreateTaskRequestDto;
 import com.tasktracker.domain.dto.TaskDto;
+import com.tasktracker.domain.dto.UpdateTaskRequestDto;
 import com.tasktracker.domain.entity.Task;
 import com.tasktracker.mapper.TaskMapper;
 import org.springframework.stereotype.Component;
@@ -16,6 +18,17 @@ public class TaskMapperImpl implements TaskMapper {
                 dto.title(),
                 dto.description(),
                 dto.dueDate(),
+                dto.priority()
+        );
+    }
+
+    @Override
+    public UpdateTaskRequest fromDto(UpdateTaskRequestDto dto) {
+        return new UpdateTaskRequest(
+                dto.title(),
+                dto.description(),
+                dto.dueDate(),
+                dto.status(),
                 dto.priority()
         );
     }
