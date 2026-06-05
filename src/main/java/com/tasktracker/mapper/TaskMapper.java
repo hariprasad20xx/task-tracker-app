933674fafcc855cd8549358b495b9c6504054375
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-05 21:03:48 +0530

    chore(mapper): add UpdateTaskRequestDto mapping to TaskMapper

--- src/main/java/com/tasktracker/mapper/TaskMapper.java
+++ src/main/java/com/tasktracker/mapper/TaskMapper.java
@@ -1,13 +1,17 @@
 package com.tasktracker.mapper;
 
 import com.tasktracker.domain.CreateTaskRequest;
+import com.tasktracker.domain.UpdateTaskRequest;
 import com.tasktracker.domain.dto.CreateTaskRequestDto;
 import com.tasktracker.domain.dto.TaskDto;
+import com.tasktracker.domain.dto.UpdateTaskRequestDto;
 import com.tasktracker.domain.entity.Task;
 
 public interface TaskMapper {
 
     CreateTaskRequest fromDto(CreateTaskRequestDto dto);
+
+    UpdateTaskRequest fromDto(UpdateTaskRequestDto dto);
 
     TaskDto toDto(Task task);
 }
